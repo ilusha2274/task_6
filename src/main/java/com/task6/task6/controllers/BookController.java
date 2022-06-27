8852c125ccf88f1b1380dfa7cd0c2fd6e36ef919
ilusha2274 <ilya.Shamanin@yandex.ru>
2022-06-28 00:23:23 +0300

    Реализовал написание книги

--- src/main/java/com/task6/task6/controllers/BookController.java
+++ src/main/java/com/task6/task6/controllers/BookController.java
@@ -1,11 +1,13 @@
 package com.task6.task6.controllers;
 
+import com.task6.task6.domain.Book;
 import com.task6.task6.repository.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
@@ -15,7 +17,19 @@ public class BookController {
 
     @GetMapping("/{id}")
     public String printBook(@PathVariable Long id, Model model) {
-        model.addAttribute("bookName", "Книга: " + bookService.getBookById(id).getName());
+        model.addAttribute("book", bookService.getBookById(id));
+
+        return "book";
+    }
+
+    @PostMapping("/{id}")
+    public String updateBook(@PathVariable Long id, Model model, String textBook){
+        Book book = bookService.getBookById(id);
+
+        book.setText(textBook);
+        bookService.saveOrUpdate(book);
+
+        model.addAttribute("book", book);
 
         return "book";
     }
